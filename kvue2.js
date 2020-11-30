6ff2fe06013248b90002c5215371c3b84e6fb07a
周林 <634023699@qq.com>
2020-11-30 11:31:06 +0800

    添加新patch

--- kvue2.js
+++ kvue2.js
@@ -3,7 +3,7 @@ function defineReactive(obj, key, val) {
     const dep = new Dep();
     Object.defineProperty(obj, key, {
         get() {
-            console.log(`get:${key},val:${val}`);
+            console.log(`get:${key},val:${val}`)
             Dep.target && dep.addDep(Dep.target);
             return val;
         },
@@ -15,36 +15,36 @@ function defineReactive(obj, key, val) {
             //     watch.update()
             // })
             dep.notity();
-            console.log(`set:${key},val:${newVal}`);
-        },
+            console.log(`set:${key},val:${newVal}`)
+        }
     });
 }
 
 function observe(obj) {
-    if (typeof obj !== "object" || obj == null) return;
-    Object.keys(obj).forEach((key) => {
+    if (typeof obj !== 'object' || obj == null) return;
+    Object.keys(obj).forEach(key => {
         defineReactive(obj, key, obj[key]);
-    });
+    })
 }
 
 function proxy(vm) {
-    Object.keys(vm.$data).forEach((key) => {
+    Object.keys(vm.$data).forEach(key => {
         Object.defineProperty(vm, key, {
             get() {
-                return vm.$data[key];
+                return vm.$data[key]
             },
             set(newVal) {
                 vm.$data[key] = newVal;
-            },
-        });
-    });
+            }
+        })
+    })
 }
+
 
 class Kvue {
     constructor(options) {
         this.$options = options;
         this.$data = options.data;
-        // this.$el = options.el;
         this.walk(this.$data);
         proxy(this);
         // new Compiler(this.$el, this)
@@ -57,33 +57,42 @@ class Kvue {
         this.$el = document.querySelector(el);
         const updateComponent = () => {
             // const { render } = this.$options;
-            // const el = render.call(this);
+            // const dom = render.call(this);
             // const parent = this.$el.parentElement;
-            // parent.insertBefore(el, this.$el.nextSibling);
+            // parent.insertBefore(dom, this.$el.nextSibling);
             // parent.removeChild(this.$el);
-            // this.$el = el;
+            // this.$el = dom;
 
             // vnode实现
             const { render } = this.$options;
+
             const vnode = render.call(this, this.$createElement);
+
+            // console.log(vnode)
             this._update(vnode);
-        };
-        new Watcher(this, updateComponent);
+        }
+
+        new Watcher(this, updateComponent)
     }
+
     $createElement(tag, props, children) {
-        return { tag, props, children };
+        return {
+            tag,
+            props,
+            children
+        }
     }
     _update(vnode) {
         const preVnode = this._vnode;
         if (!preVnode) {
             // 初始化过程
-            this._patch_(this.$el, vnode);
+            this._patch(this.$el, vnode);
         } else {
             // 更新
-            this._patch_(preVnode, vnode);
+            this._patch(preVnode, vnode);
         }
     }
-    _patch_(oldVnode, vnode) {
+    _patch(oldVnode, vnode) {
 
         if (oldVnode.nodeType) {
             // 初始化过程
@@ -134,48 +143,61 @@ class Kvue {
                 }
             }
         }
+
     }
+
     createElm(vnode) {
-        const el = document.createElement(vnode.tag);
-        // props
-        if (vnode.props) {
-            for (const key in vnode.props) {
-                el.setAttribute(key, vnode.props[key]);
-            }
-        }
-        // children
-        if (vnode.children) {
-            if (typeof vnode.children == "string") {
-                el.textContent = vnode.children;
-            } else {
-                vnode.children.forEach((v) => {
-                    const child = this.createElm(v);
-                    el.appendChild(child);
-                });
-            }
-        }
+            // console.log('-----')
+            const el = document.createElement(vnode.tag);
+            // props
+            // if (vnode.props) {
+            //     Object.keys(vnode.props).forEach(key => {
+            //         el.setAttribute(key, vnode.props[key]);
+            //     })
+            // }
 
-        // 保存vnode
-        vnode.el = el;
-        return el;
-    }
+            if (vnode.props) {
+                for (const key in vnode.props) {
+                    el.setAttribute(key, vnode.props[key]);
+                }
+            }
 
-    // 更新孩子
+            // if (vnode.children) {
+            //     if (typeof vnode.children == 'string') {
+            //         el.textContent = vnode.children;
+            //     }
+            // }
+            if (vnode.children) {
+                if (typeof vnode.children == "string") {
+                    el.textContent = vnode.children;
+                } else {
+                    vnode.children.forEach((v) => {
+                        const child = this.createElm(v);
+                        el.appendChild(child);
+                    });
+                }
+            }
+
+            vnode.el = el;
+            return el;
+        }
+        // 更新孩子
     updateChildren(el, oldCh, newCh) {
         const len = Math.min(oldCh.length, newCh.length);
         // 遍历较短的那个子素组
         for (let i = 0; i < len; i++) {
-            this._patch_(oldCh[i], newCh[i]);
+            this._patch(oldCh[i], newCh[i]);
         }
     }
     walk(obj) {
         if (Array.isArray(obj)) {
-            console.log("===处理数组====");
+            console.log('===处理数组====')
         } else {
-            observe(obj);
+            observe(obj)
         }
     }
 }
+
 
 class Compiler {
     constructor(el, vm) {
@@ -187,58 +209,55 @@ class Compiler {
     }
     compile(el) {
         const childNodes = el.childNodes;
-        Array.from(childNodes).forEach((node) => {
+        Array.from(childNodes).forEach(node => {
             if (this.isElement(node)) {
                 const nodeAttrs = node.attributes;
-                Array.from(nodeAttrs).forEach((attr) => {
+                Array.from(nodeAttrs).forEach(attr => {
                     const attrName = attr.name;
                     const attrVal = attr.value;
                     if (this.isDirective(attrName)) {
                         const dir = attrName.substring(2);
-                        this[dir] && this[dir](node, attrVal);
+                        this[dir] && this[dir](node, attrVal)
                     } else if (this.isEvent(attrName)) {
                         const eventType = attrName.substring(1);
                         this.eventHandle(node, eventType, attrVal);
                     }
+
                 });
                 // this.isDirective(nod)
             } else if (this.isIner(node)) {
-                this.conpileText(node, RegExp.$1);
+                this.conpileText(node, RegExp.$1)
             }
             if (node.childNodes) {
                 this.compile(node);
             }
-        });
+        })
     }
     isElement(node) {
-        return node.nodeType == 1;
+        return node.nodeType == 1
     }
     text(node, exp) {
         // node.textContent = this.$vm[exp]
         // this.textUpdater(node, val);
-        this.update(node, exp, "text");
+        this.update(node, exp, 'text');
     }
     update(node, exp, type) {
-        const fn = this[type + "Updater"];
+        const fn = this[type + 'Updater'];
         fn && fn(node, this.$vm[exp]);
         new Watcher(this.$vm, exp, function(val) {
             fn && fn(node, val);
-        });
+        })
     }
     model(node, exp) {
-        this.update(node, exp, "model");
-        node.addEventListener(
-            "input",
-            (e) => {
-                this.$vm[exp] = e.target.value;
-            },
-            false
-        );
+        this.update(node, exp, 'model');
+        node.addEventListener('input', (e) => {
+            this.$vm[exp] = e.target.value
+        }, false)
     }
     html(node, exp) {
         // node.innerHTML = this.$vm[exp]
         // this.htmlUpdater(node, val);
-        this.update(node, exp, "html");
+        this.update(node, exp, 'html');
     }
     textUpdater(node, val) {
         node.textContent = val;
@@ -251,17 +270,17 @@ class Compiler {
         node.value = val;
     }
     isIner(node) {
-        return node.nodeType == 3 && /\{\{(.*)\}\}/.test(node.textContent);
+        return node.nodeType == 3 && /\{\{(.*)\}\}/.test(node.textContent)
     }
     conpileText(node, exp) {
         // node.textContent = this.$vm[exp];
-        this.update(node, exp, "text");
+        this.update(node, exp, 'text');
     }
     isDirective(attrName) {
-        return attrName.startsWith("v-");
+        return attrName.startsWith('v-')
     }
     isEvent(attrName) {
-        return attrName.startsWith("@");
+        return attrName.startsWith('@')
     }
     eventHandle(node, eventType, attrVal) {
         const { methods } = this.$vm.$options;
@@ -270,31 +289,32 @@ class Compiler {
     }
 }
 
+
 // const watchers = [];
 class Dep {
     constructor() {
-        this.watchers = new Set();
+        this.watchers = new Set()
     }
     addDep() {
-        this.watchers.add(Dep.target);
+        this.watchers.add(Dep.target)
     }
     notity() {
-        this.watchers.forEach((watch) => watch.update());
+        this.watchers.forEach(watch => watch.update())
     }
 }
 class Watcher {
-    constructor(vm, expOrfn) {
+    constructor(vm, expOrFn) {
         this.$vm = vm;
-        this.updateFn = expOrfn;
+        this.updateFn = expOrFn;
         this.get();
     }
     get() {
         Dep.target = this;
-        this.updateFn(this.$vm);
+        this.updateFn();
         Dep.target = null;
     }
     update() {
-        // this.updateFn.call(this.$vm);
         this.get();
     }
+
 }
